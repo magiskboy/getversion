776ed895e160b27361a09986f9d3a3f69759e3e0
Nguyen Khac Thanh <nguyenkhacthanh244@gmail.com>
2021-01-25 12:58:06 +0700

    add table in the result

--- getversion.sh
+++ getversion.sh
@@ -2,15 +2,20 @@
 
 source lib.sh
 
-
 function getversion() {
-    printf "%-20s | %-20s\n" "Package" "Version"
-    echo "---------------------|--------------------"
+    [[ -z GETVERSION_WIDTH_NAME ]] && GETVERSION_WIDTH_NAME=20
+    [[ -z GETVERSION_WIDTH_VERSION ]] && GETVERSION_WIDTH_VERSION=20
 
-    for p in $@; do
-        version=$(eval '_$p 2>/dev/null')
+    printf "%-${GETVERSION_WIDTH_NAME}s | %-${GETVERSION_WIDTH_VERSION}s\n" "Package" "Version"
+
+    namepad=$(printf '%*s' "$GETVERSION_WIDTH_NAME") && namepad=${namepad// /-}
+    versionpad=$(printf '%*s' "$GETVERSION_WIDTH_VERSION") && versionpad=${versionpad// /-}
+    printf "%s-|-%s\n" $namepad $versionpad
+
+    for name in $@; do
+        version=$(eval '_$name 2>/dev/null')
         if [[ $? -eq 0 ]]; then
-            printf "%-20s | %-20s\n" $p $version
+            printf "%-${GETVERSION_WIDTH_NAME}s | %-${GETVERSION_WIDTH_VERSION}s\n" $name $version
         fi
     done
 }
